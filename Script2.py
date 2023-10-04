8754e0a3ef0ec16822282910cc255f9061b6e37a
SuchithG <suchithgowda1995@gmail.com>
2023-10-04 21:37:00 +0530

    data transformation

---
+++ Script2.py
@@ -0,0 +1,89 @@
+import os
+import pandas as pd
+import time
+
+start_time = time.time()
+
+folder_paths = [
+    "path1",
+    "path2",
+    "path3",
+    "path4",
+    "path5"
+]
+
+output_folder_path = "output_path"
+
+if not os.path.exists(output_folder_path):
+    os.makedirs(output_folder_path)
+    print(f"Created output folder at: {output_folder_path}")
+
+dfs = []
+
+for folder_path in folder_paths:
+    
+    # Check if folder path exists
+    if not os.path.exists(folder_path):
+        print(f"Folder path '{folder_path}' does not exist. Skipping...")
+        continue
+    
+    print(f"Processing files in folder: {folder_path}")
+
+    # Loop through each file in the folder
+    for filename in os.listdir(folder_path):
+        
+        # Check if the file ends with .xlsm.xlsx and is not a temporary file
+        if filename.endswith(".xlsm.xlsx") and not filename.startswith("~$"):
+            
+            # Define the full path to the file
+            file_path = os.path.join(folder_path, filename)
+            
+            # Try reading the file with header
+            try:
+                df = pd.read_excel(file_path)
+                
+                # Check if necessary columns are in the file
+                if set(["Formula", "Resource name", "Date", "Month"]).issubset(df.columns):
+                    
+                    # Keep only rows where necessary columns are not null
+                    df = df[df[["Formula", "Resource name", "Date", "Month"]].notnull().all(axis=1)]
+                    
+                    if not df.empty:
+                        dfs.append(df)
+                        print(f"Added data from file: {filename}")
+                    else:
+                        print(f"File {filename} does not contain valid data. Skipping...")
+            
+            except Exception as e:
+                print(f"Could not read file {filename} with header. Error: {e}")
+                try:
+                    pd.read_excel(file_path, header=None)
+                    print(f"File {filename} read without header. Skipping...")
+                except Exception as e:
+                    print(f"Could not read file {filename} without header. Error: {e}")
+                    pass
+
+# After concatenating all data frames
+if dfs:
+    result_df = pd.concat(dfs, ignore_index=True)
+
+    # Replace NaN values with 0
+    result_df.fillna(0, inplace=True)
+
+    # Transpose specified columns
+    result_df = result_df.melt(id_vars=['Formula', 'Resource name', 'Date', 'Month', 'Region', 'PDF Name', 'Asset Class/Reports', 'Application', '2 eye', '4 eye', 'Error Count', 'Actual Date of upload'], 
+                               value_vars=["Setup", "Amend", "Review", "Closure ", "Deletion", "Exceptions"], 
+                               var_name='Name', 
+                               value_name='Value')
+
+    # Save the concatenated data frame to a new Excel file
+    output_file_path = os.path.join(output_folder_path, "concatenated_data.xlsx")
+    result_df.to_excel(output_file_path, index=False)
+    print(f"Data concatenated successfully. Output file saved at: {output_file_path}")
+else:
+    print("No valid data found. Concatenation skipped.")
+
+end_time = time.time()
+
+execution_time_in_minutes = (end_time - start_time) / 60
+print(f"Script execution in {execution_time_in_minutes:.2f} minutes")
