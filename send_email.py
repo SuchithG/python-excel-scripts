8bf9b86028d7ee4cac58104676fb0d7a7bcae2d8
SuchithG <suchithgowda1995@gmail.com>
2023-11-01 23:03:50 +0530

    feat: :sparkles: process of sending an email with an attachment using Python's smtplib and email libraries.

---
+++ send_email.py
@@ -0,0 +1,36 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+def send_email(subject, body, to_email, attachment_path):
+    from_email = "your_email@gmail.com"
+    password = "your_password"  # Consider using an app-specific password if using Gmail
+
+    msg = MIMEMultipart()
+    msg["From"] = from_email
+    msg["To"] = to_email
+    msg["Subject"] = subject
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    with open(attachment_path, "rb") as attachment:
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        part.add_header("Content-Disposition", f"attachment; filename= {attachment_path}")
+        msg.attach(part)
+
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(from_email, password)
+        server.sendmail(from_email, to_email, msg.as_string())
+        server.quit()
+        print("Email sent successfully!")
+    except Exception as e:
+        print(f"Error occurred: {e}")
+
+# Example usage:
+# send_email("Test Subject", "This is the body of the email", "recipient_email@gmail.com", "/path/to/excel_workbook.xlsx")
