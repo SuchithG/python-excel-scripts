8974fbf52b29064d699a0de881672cf8ed5bd827
SuchithG <suchithgowda1995@gmail.com>
2023-11-01 23:06:56 +0530

    feat: :sparkles: send an email with the "2 eye count" table embedded in the body and an attached Excel workbook

--- send_email.py
+++ send_email.py
@@ -3,15 +3,47 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
+from email import encoders
+import pandas as pd
 
-def send_email(subject, body, to_email, attachment_path):
+# Load your data (assuming you have it in an Excel file)
+df = pd.read_excel("/path/to/your/excel_file.xlsx")
+
+# Process your data to get the '2 eye count' table (as we did earlier)
+aggregated_data = df.groupby(['Date', '2 eye']).agg({
+    'Setup': 'sum',
+    'Amend': 'sum',
+    'Closure': 'sum',
+    'Deletion': 'sum',
+    'Exceptions': 'sum',
+}).reset_index()
+aggregated_data.rename(columns={'2 eye': 'Name'}, inplace=True)
+aggregated_data['2 eye Count'] = aggregated_data[['Setup', 'Amend', 'Closure', 'Deletion', 'Exceptions']].sum(axis=1)
+
+def send_email_with_table(subject, body, to_email, attachment_path):
     from_email = "your_email@gmail.com"
     password = "your_password"  # Consider using an app-specific password if using Gmail
+
+    # Convert DataFrame to HTML table
+    table_html = df.to_html()
 
     msg = MIMEMultipart()
     msg["From"] = from_email
     msg["To"] = to_email
     msg["Subject"] = subject
+
+    body = f"""
+    <html>
+        <head></head>
+        <body>
+            <p>Hi,</p>
+            <p>Here's the "2 eye count" table:</p>
+            {table_html}
+            <p>Regards,</p>
+            <p>Your Name</p>
+        </body>
+    </html>
+    """
 
     msg.attach(MIMEText(body, 'plain'))
 
@@ -32,5 +64,6 @@ def send_email(subject, body, to_email, attachment_path):
     except Exception as e:
         print(f"Error occurred: {e}")
 
-# Example usage:
-# send_email("Test Subject", "This is the body of the email", "recipient_email@gmail.com", "/path/to/excel_workbook.xlsx")
+# Send the email
+response = send_email_with_table("Subject of Email", aggregated_data, "recipient_email@example.com", "/path/to/excel_workbook.xlsx")
+print(response)
