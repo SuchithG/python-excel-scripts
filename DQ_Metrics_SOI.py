5300f11dd4cd7ece68b9183b1b0fa83616f3cca3
SuchithG <suchithgowda1995@gmail.com>
2023-11-09 23:46:15 +0530

    feat: :art: selecting the previous month's file based on the current date

--- DQ_Metrics_SOI.py
+++ DQ_Metrics_SOI.py
@@ -1,14 +1,22 @@
 import pandas as pd
+from datetime import datetime, timedelta
 
-# Define the paths to the Excel files
-dq_exception_path = 'path/to/DQ_CDE_SOI_23_Oct 23.xlsx'
-asset_class_dict_path = 'path/to/DQ CDE Dictionary.xlsx'
+# Define the base paths for the files
+base_path_exceptions = '/path/to/exceptions/folder/'
+base_path_dictionaries = '/path/to/dictionaries/folder/'
 
-# Load the 'DQ Exception' sheet from the first Excel file
-dq_exception_df = pd.read_excel(dq_exception_path, sheet_name='DQ Exception')
+# Get the previous month's file name based on the current date
+previous_month = (datetime.now().replace(day=1) - timedelta(days=1)).strftime("%b %y")
+dq_exception_file_name = f"DQ_CDE_SOI_{previous_month}.xlsx"
 
-# Load the 'Asset Class Dictionary' sheet from the second Excel file
+# Construct the full paths for the files
+dq_exception_file_path = f"{base_path_exceptions}{dq_exception_file_name}"
+asset_class_dict_path = f"{base_path_dictionaries}DQ CDE Dictionary.xlsx"
+
+# Load the 'DQ Exception' sheet and the 'Asset Class Dictionary' sheet
+dq_exception_df = pd.read_excel(dq_exception_file_path, sheet_name='DQ Exception')
 asset_class_dict_df = pd.read_excel(asset_class_dict_path, sheet_name='Asset Class Dictionary')
+
 
 # Perform an inner join on the 'MSG_TYP' column
 merged_df = pd.merge(dq_exception_df, asset_class_dict_df, on='MSG_TYP', how='inner')
