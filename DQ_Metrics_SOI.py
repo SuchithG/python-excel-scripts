ec37a426c1998a0289244e2a8f65085bce3c3200
SuchithG <suchithgowda1995@gmail.com>
2023-11-10 03:15:28 +0530

    sorting date

--- DQ_Metrics_SOI.py
+++ DQ_Metrics_SOI.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from datetime import datetime, timedelta
 
@@ -83,3 +84,35 @@ output_file_path = f'/desired/path/to/output/folder/{output_file_name}'
 grouped_df.to_excel(output_file_path, index=False)
 
 print(f"DataFrame has been saved to {output_file_path}")
+
+# Define the source directory where all Excel files are located
+source_directory = '/path/to/source/folder/'
+
+# Define the target directory where the concatenated file will be saved
+target_directory = '/path/to/target/folder/'
+
+# List all Excel files in the source directory
+excel_files = [file for file in os.listdir(source_directory) if file.endswith('.xlsx')]
+
+# Read each Excel file into a DataFrame and store them in a list
+df_list = [pd.read_excel(os.path.join(source_directory, file)) for file in excel_files]
+
+# Concatenate all DataFrames into one
+concatenated_df = pd.concat(df_list, ignore_index=True)
+
+# Convert 'Month' column to datetime for sorting
+concatenated_df['Month'] = pd.to_datetime(concatenated_df['Month'], format='%B-%Y')
+
+# Sort the DataFrame based on the datetime 'Month' column
+concatenated_df.sort_values(by='Month', inplace=True)
+
+# Convert the datetime 'Month' column back to the desired string format if needed
+concatenated_df['Month'] = concatenated_df['Month'].dt.strftime('%B-%Y')
+
+# Define the output file name for the concatenated DataFrame
+output_file_name_concat = "DQ_Metrics_SOI_UAT.xlsx"
+
+# Save the concatenated DataFrame to a new Excel file in the target folder
+concatenated_df.to_excel(os.path.join(target_directory, output_file_name_concat), index=False)
+
+print(f"All Excel files have been concatenated and saved to {os.path.join(target_directory, output_file_name_concat)}")
