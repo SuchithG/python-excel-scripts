2f865a4aae4e738642f65dce715de9f81f83b60e
SuchithG <suchithgowda1995@gmail.com>
2023-11-15 21:48:30 +0530

    feat: :art:

--- Exception_Trends.py
+++ Exception_Trends.py
@@ -1,71 +1,28 @@
 import pandas as pd
-import datetime
-import os
 
-# Function to get the previous month's file name for the input file
-def get_previous_month_file_name():
-    today = datetime.date.today()
-    first_day_of_current_month = today.replace(day=1)
-    last_day_of_previous_month = first_day_of_current_month - datetime.timedelta(days=1)
-    file_date = last_day_of_previous_month.strftime('%b_%Y').upper()
-    return f"uRDSandFDW_{file_date}.xlsx"
+input_file_path = 'path_to_your_input_excel_file.xlsx'
 
-# Function to read a sheet from the Excel file
-def read_sheet(file_path, sheet_name):
-    try:
-        df = pd.read_excel(file_path, sheet_name=sheet_name)
-        print(f"Sheet '{sheet_name}' read successfully.")
-        return df
-    except Exception as e:
-        print(f"Error reading '{sheet_name}': {e}")
-        return None
+try:
+    # Define the columns to read
+    columns_to_read = ["MSG_TYP", "PRIORITY", "STATUS"]
 
-# Function to extract and combine MSG_TYP values from Q1 Deal and Q1 Tranche
-def combine_msg_typ_values(q1_deal, q1_tranche):
-    msg_typ_deal = q1_deal['MSG_TYP']
-    msg_typ_tranche = q1_tranche['MSG_TYP']
-    combined_msg_typ = pd.concat([msg_typ_deal, msg_typ_tranche]).drop_duplicates().reset_index(drop=True)
-    return combined_msg_typ
+    # Read specific columns from the sheets
+    q1_deal = pd.read_excel(input_file_path, sheet_name='Q1 Deal', usecols=columns_to_read)
+    q1_tranche = pd.read_excel(input_file_path, sheet_name='Q1 Tranche', usecols=columns_to_read)
 
-# Function to get the output file name based on the previous month
-def get_output_file_name():
-    today = datetime.date.today()
-    first_day_of_current_month = today.replace(day=1)
-    last_day_of_previous_month = first_day_of_current_month - datetime.timedelta(days=1)
-    file_date = last_day_of_previous_month.strftime('%b-%y').upper()  # Format as Mon-YY
-    return f"ExceptionTrends_{file_date}_script_output.xlsx"
+    # Concatenate the data from both sheets
+    combined_data = pd.concat([q1_deal, q1_tranche])
 
-# Function to write data to a new Excel file
-def write_to_excel(df, folder_path, sheet_name):
-    file_name = get_output_file_name()
-    file_path = os.path.join(folder_path, file_name)
+    # Create the output DataFrame
+    output_df = combined_data.copy()
 
-    try:
-        with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
-            df.to_excel(writer, sheet_name=sheet_name, index=False)
-        print(f"Data written successfully to sheet '{sheet_name}' in '{file_path}'.")
-    except Exception as e:
-        print(f"Error writing to Excel: {e}")
+    # Perform any additional processing if needed
 
-# Main script execution
-input_folder_path = 'path_to_input_folder'  # Replace with actual path
-output_folder_path = 'path_to_output_folder'  # Replace with actual path
+    # Write to output file
+    output_file_path = 'path_to_your_output_excel_file.xlsx'
+    output_df.to_excel(output_file_path, index=False)
 
-input_file_name = get_previous_month_file_name()
-input_file_path = os.path.join(input_folder_path, input_file_name)
+    print("Data processed and written to output file.")
 
-q1_deal = read_sheet(input_file_path, "Q1 Deal")
-q1_tranche = read_sheet(input_file_path, "Q1 Tranche")
-
-if q1_deal is not None and q1_tranche is not None:
-    combined_msg_typ = combine_msg_typ_values(q1_deal, q1_tranche)
-
-    output_data = {
-        'MSG_TYP': combined_msg_typ,
-        # Other columns can be added here with initialization
-    }
-    output_df = pd.DataFrame(output_data)
-
-    write_to_excel(output_df, output_folder_path, 'Processed Data')
-else:
-    print("Required input sheets are missing.")
+except Exception as e:
+    print(f"An error occurred: {e}")
