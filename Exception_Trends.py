17829d7c13f30547c3683a709da39a97fd0a2dbd
SuchithG <suchithgowda1995@gmail.com>
2023-11-15 18:40:28 +0530

    feat: :art:

--- Exception_Trends.py
+++ Exception_Trends.py
@@ -2,7 +2,7 @@ import pandas as pd
 import datetime
 import os
 
-# Function to get the previous month's file name
+# Function to get the previous month's file name for the input file
 def get_previous_month_file_name():
     today = datetime.date.today()
     first_day_of_current_month = today.replace(day=1)
@@ -20,80 +20,52 @@ def read_sheet(file_path, sheet_name):
         print(f"Error reading '{sheet_name}': {e}")
         return None
 
-# Process the data here (Placeholder functions)
-def process_q1_q2_deal(df):
-    # Add your processing steps here
-    pass
+# Function to extract and combine MSG_TYP values from Q1 Deal and Q1 Tranche
+def combine_msg_typ_values(q1_deal, q1_tranche):
+    msg_typ_deal = q1_deal['MSG_TYP']
+    msg_typ_tranche = q1_tranche['MSG_TYP']
+    combined_msg_typ = pd.concat([msg_typ_deal, msg_typ_tranche]).drop_duplicates().reset_index(drop=True)
+    return combined_msg_typ
 
-def process_q1_q2_tranche(df):
-    # Add your processing steps here
-    pass
-
-# Function to get the current month's output file name
+# Function to get the output file name based on the previous month
 def get_output_file_name():
     today = datetime.date.today()
-    file_date = today.strftime('%b-%y').upper()
+    first_day_of_current_month = today.replace(day=1)
+    last_day_of_previous_month = first_day_of_current_month - datetime.timedelta(days=1)
+    file_date = last_day_of_previous_month.strftime('%b-%y').upper()  # Format as Mon-YY
     return f"ExceptionTrends_{file_date}_script_output.xlsx"
 
-# Function to write data to an Excel file
+# Function to write data to a new Excel file
 def write_to_excel(df, folder_path, sheet_name):
     file_name = get_output_file_name()
     file_path = os.path.join(folder_path, file_name)
 
     try:
-        with pd.ExcelWriter(file_path, mode='a', engine='openpyxl', if_sheet_exists='replace') as writer:
+        with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
             df.to_excel(writer, sheet_name=sheet_name, index=False)
         print(f"Data written successfully to sheet '{sheet_name}' in '{file_path}'.")
     except Exception as e:
         print(f"Error writing to Excel: {e}")
 
-# Placeholder function to process and combine data from input sheets
-def process_data(q1_deal, q2_deal, q1_tranche, q2_tranche):
-    # Example processing - modify this as per your actual data and requirements
-    # Here, we're just creating a dummy DataFrame with the specified columns
-    data = {
-        'Exception trend': [],  # Populate with actual data
-        'MSG_TYP': [],          # Populate with actual data
-        'Message Type Group': [],  # Populate with actual data
-        'Attribute Count': [],     # This might be a count of unique values in a column
-        'PRIORITY': [],            # Populate with actual data
-        'Volume': [],              # Sum or count of a certain column
-        'Total': [],               # Sum or count of a certain column
-        'Month/Year': datetime.date.today().strftime('%b/%Y'),  # Current Month/Year
-        'STATUS': [],              # Populate with actual data
-        'Priority Count': []       # This might be a count based on priority
-    }
-
-    # Create a DataFrame from the processed data
-    processed_df = pd.DataFrame(data)
-
-    return processed_df
-
 # Main script execution
-input_folder_path = 'path_to_input_folder'
-output_folder_path = 'path_to_output_folder'
+input_folder_path = 'path_to_input_folder'  # Replace with actual path
+output_folder_path = 'path_to_output_folder'  # Replace with actual path
 
 input_file_name = get_previous_month_file_name()
 input_file_path = os.path.join(input_folder_path, input_file_name)
 
-# Reading each sheet
 q1_deal = read_sheet(input_file_path, "Q1 Deal")
-q2_deal = read_sheet(input_file_path, "Q2 Deal")
 q1_tranche = read_sheet(input_file_path, "Q1 Tranche")
-q2_tranche = read_sheet(input_file_path, "Q2 Tranche")
 
-# Process each DataFrame as needed
-# Replace these with your actual processing functions
-processed_data = {
-    'Q1 Deal': process_q1_q2_deal(q1_deal) if q1_deal is not None else None,
-    'Q2 Deal': process_q1_q2_deal(q2_deal) if q2_deal is not None else None,
-    'Q1 Tranche': process_q1_q2_tranche(q1_tranche) if q1_tranche is not None else None,
-    'Q2 Tranche': process_q1_q2_tranche(q2_tranche) if q2_tranche is not None else None
-}
+if q1_deal is not None and q1_tranche is not None:
+    combined_msg_typ = combine_msg_typ_values(q1_deal, q1_tranche)
 
-# Process the data from the sheets
-processed_df = process_data(q1_deal, q2_deal, q1_tranche, q2_tranche)
+    output_data = {
+        'MSG_TYP': combined_msg_typ,
+        # Other columns can be added here with initialization
+    }
+    output_df = pd.DataFrame(output_data)
 
-# Write the processed data to the output Excel file
-if processed_df is not None:
-    write_to_excel(processed_df, output_folder_path, 'Processed Data')
+    write_to_excel(output_df, output_folder_path, 'Processed Data')
+else:
+    print("Required input sheets are missing.")
