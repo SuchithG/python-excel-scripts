842a59a3c72de2b91ecbca74827344d17daf838f
SuchithG <suchithgowda1995@gmail.com>
2023-11-23 15:38:13 +0530

    feat: :art:

--- ExceptionTrends.py
+++ ExceptionTrends.py
@@ -12,31 +12,23 @@ df['OPEN_DT'] = pd.to_datetime(df['OPEN_DT'], errors='coerce')
 # Use OPEN_DT for the Month/Year value for all records
 df['Month/Year'] = df['OPEN_DT'].dt.strftime('%b-%y')
 
-# Group by MSG_TYP, PRIORITY, STATUS, and Month/Year
+# Group by MSG_TYP, PRIORITY, STATUS, and Month/Year, and count occurrences
 grouped = df.groupby(['MSG_TYP', 'PRIORITY', 'STATUS', 'Month/Year'])
 
 # Count the number of occurrences in each group
-output_df = grouped.size().reset_index(name='Count')
+output_df = grouped.size().reset_index(name='Volume')  # Using 'Volume' here, can be duplicated for 'Status_Count' if needed
 
-# Create Status_Count and Volume columns (assuming they are the same)
-output_df['Status_Count'] = output_df['Count']
-output_df['Volume'] = output_df['Count']
+# Create Status_Count column, assuming it is the same as Volume
+output_df['Status_Count'] = output_df['Volume']
 
-# Group by 'Month/Year' and 'PRIORITY' and count distinct 'ATTR_NME'
-grouped_by_month_priority = df.groupby(['Month/Year', 'PRIORITY'])['ATTR_NME'].nunique().reset_index(name='Priority_Count')
+# Now, count distinct ATTR_NME for each MSG_TYP
+distinct_attr_count = df.groupby('MSG_TYP')['ATTR_NME'].nunique().reset_index(name='Attribute Count')
 
-# Now to replicate the summarization from Alteryx for distinct counts of ATTR_NME
-# Group by MSG_TYP and count distinct ATTR_NME
-distinct_attr = df.groupby('MSG_TYP')['ATTR_NME'].nunique().reset_index(name='CountDistinct_ATTR_NME')
+# Merge the distinct attribute count with the main DataFrame
+# Assuming 'MSG_TYP' is unique in the distinct_attr_count DataFrame
+# If not, you might need to adjust the merging strategy
+combined_df = pd.merge(output_df, distinct_attr_count, on='MSG_TYP', how='left')
 
-# Print the grouped DataFrame with counts for each group
-print("Grouped DataFrame by MSG_TYP, PRIORITY, STATUS, and Month/Year:")
-print(output_df)
-
-# Print the grouped DataFrame by Month/Year and PRIORITY with distinct counts of ATTR_NME
-print("\nGrouped DataFrame by Month/Year and PRIORITY with distinct counts of ATTR_NME:")
-print(grouped_by_month_priority)
-
-# Print the distinct count of ATTR_NME per MSG_TYP
-print("\nDistinct Count of ATTR_NME per MSG_TYP:")
-print(distinct_attr)
+# Print the final DataFrame that matches the Alteryx output
+print("Combined DataFrame:")
+print(combined_df)
