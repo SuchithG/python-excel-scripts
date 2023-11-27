a31dfa374f51f6ea9d55b7fa0abcb58d0c763b6a
SuchithG <suchithgowda1995@gmail.com>
2023-11-27 23:22:17 +0530

    fix:

--- ExceptionTrends.py
+++ ExceptionTrends.py
@@ -142,37 +142,18 @@ ordered_columns = [
 ]
 final_combined_df = final_combined_df[ordered_columns]
 
-output_folderpath = "outputfolderpath/"
-concatenated_folderpath = "concatenatedfolderpath/"
+# Specify the output folder path and file name
+output_folder_path = '/path/to/your/output/folder/'
+output_file_name = 'final_output.xlsx'
 
-# Define the source directory where all Excel files are located
-source_directory = '/path/to/source/folder/'
+# Create the full file path
+full_file_path = os.path.join(output_folder_path, output_file_name)
 
-# Define the target directory where the concatenated file will be saved
-target_directory = '/path/to/target/folder/'
+# Check if the output folder exists, and if not, create it
+if not os.path.exists(output_folder_path):
+    os.makedirs(output_folder_path)
 
-# List all Excel files in the source directory
-excel_files = [file for file in os.listdir(source_directory) if file.endswith('.xlsx')]
+# Save the DataFrame to an Excel file
+final_combined_df.to_excel(full_file_path, index=False)
 
-# Read each Excel file into a DataFrame and store them in a list
-df_list = [pd.read_excel(os.path.join(source_directory, file)) for file in excel_files]
-
-# Concatenate all DataFrames into one
-concatenated_df = pd.concat(df_list, ignore_index=True)
-
-# Convert 'Month' column to datetime for sorting
-concatenated_df['Month'] = pd.to_datetime(concatenated_df['Month'], format='%B-%Y')
-
-# Sort the DataFrame based on the datetime 'Month' column
-concatenated_df.sort_values(by='Month', inplace=True)
-
-# Convert the datetime 'Month' column back to the desired string format if needed
-concatenated_df['Month'] = concatenated_df['Month'].dt.strftime('%B-%Y')
-
-# Define the output file name for the concatenated DataFrame
-output_file_name_concat = "DQ_Metrics_SOI_UAT.xlsx"
-
-# Save the concatenated DataFrame to a new Excel file in the target folder
-concatenated_df.to_excel(os.path.join(target_directory, output_file_name_concat), index=False)
-
-print(f"All Excel files have been concatenated and saved to {os.path.join(target_directory, output_file_name_concat)}")
+print(f"File saved successfully to {full_file_path}")
