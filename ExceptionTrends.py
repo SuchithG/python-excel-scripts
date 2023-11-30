1edff73110df339ccfe2675341f4bcf0617c1a92
SuchithG <suchithgowda1995@gmail.com>
2023-11-30 16:16:34 +0530

    feat: :art:

--- ExceptionTrends.py
+++ ExceptionTrends.py
@@ -1,201 +1,83 @@
-import os
 import pandas as pd
+from itertools import product
 from datetime import datetime, timedelta
 
-def get_opposite_status(existing_status):
-    if 'CLOSED' in existing_status:
-        return 'OPEN'
-    return 'CLOSED'
-
+# Define previous month details
 previous_month_date = (datetime.now().replace(day=1) - timedelta(days=1))
 previous_month_name = previous_month_date.strftime("%b_%Y").upper()
 
-# Replace 'path_to_your_excel_file.xlsx' with the actual path to your Excel file
-file_path = 'path_to_your_excel_file.xlsx'
-
-# Load the data from the Excel file's "Q1 Deal" sheet
+# Load the data from the Excel file
+file_path = 'path_to_your_excel_file.xlsx'  # Replace with the actual path
 df_q1 = pd.read_excel(file_path, sheet_name='Q1 Deal')
-
-# Load the "COUNT(*)" column from the "Q2 Deal" sheet
 df_q2 = pd.read_excel(file_path, sheet_name='Q2 Deal', usecols=['COUNT(*)'])
 
-# Get the first value from the "COUNT(*)" column of "Q2 Deal" sheet (assuming it's the same for all records)
-count_value = df_q2['COUNT(*)'].iloc[0]
+# Get the first value from the "COUNT(*)" column
+total_value = df_q2['COUNT(*)'].iloc[0]
 
-# Ensure the OPEN_DT column in "Q1 Deal" is in datetime format
+# Process the data
 df_q1['OPEN_DT'] = pd.to_datetime(df_q1['OPEN_DT'], errors='coerce')
-
-# Use OPEN_DT for the Month/Year value for all records
-df_q1['Month/Year'] = df_q1['OPEN_DT'].dt.strftime('%b-%y')
 
 msg_typ_rename = {
     '3DS': '3D Static',
     'BF': 'Business Finance',
-    'BBG': 'Bloomberg'
+    'BBG': 'Bloomberg',
+    # Add other mappings as needed
 }
-
 df_q1['MSG_TYP'] = df_q1['MSG_TYP'].map(msg_typ_rename).fillna(df_q1['MSG_TYP'])
 
-# Group by MSG_TYP, PRIORITY, STATUS, and Month/Year, and count occurrences
-grouped = df_q1.groupby(['MSG_TYP', 'PRIORITY', 'STATUS', 'Month/Year'])
+# Group and count
+grouped = df_q1.groupby(['MSG_TYP', 'PRIORITY', 'STATUS']).size().reset_index(name='Volume')
 
-# Count the number of occurrences in each group
-output_df = grouped.size().reset_index(name='Volume')
+# Calculate the Attribute Count for each MSG_TYP
+attribute_count_per_msg_type = df_q1.groupby('MSG_TYP')['ATTR_NME'].nunique().reset_index(name='Attribute Count')
 
-# Create Status_Count column, assuming it is the same as Volume
-output_df['Status_Count'] = output_df['Volume']
-
-# Now, count distinct ATTR_NME for each MSG_TYP
-distinct_attr_count = df_q1.groupby('MSG_TYP')['ATTR_NME'].nunique().reset_index(name='Attribute Count')
-
-# Calculate Priority Count which is the count of distinct ATTR_NME for a given PRIORITY across all MSG_TYP
+# Calculate the Priority Count for each PRIORITY
 priority_count = df_q1.groupby('PRIORITY')['ATTR_NME'].nunique().reset_index(name='Priority Count')
 
-# Merge the distinct attribute count with the main DataFrame
-combined_df = pd.merge(output_df, distinct_attr_count, on='MSG_TYP', how='left')
+# Define MSG_TYP and PRIORITY categories
+all_msg_types = ["3D Static", "Bloomberg", "Business Finance", "Intex", "Paragon", "STS", "TAS"]
+all_priorities = ['P1', 'P2', 'P3']
+status_types = ['OPEN', 'CLOSED']
 
-# Merge the priority count with the main DataFrame
-combined_df = pd.merge(combined_df, priority_count, on='PRIORITY', how='left')
+# Create a dataframe with all combinations of MSG_TYP, PRIORITY, and STATUS
+all_combinations = pd.DataFrame(product(all_msg_types, all_priorities, status_types), columns=['MSG_TYP', 'PRIORITY', 'STATUS'])
 
-# Add column 'Count(*)' with the value from "Q2 Deal" sheet
-combined_df['Count(*)'] = count_value
+# Merge with the existing dataframe to ensure all combinations are present
+full_combined_df = pd.merge(all_combinations, grouped, on=['MSG_TYP', 'PRIORITY', 'STATUS'], how='left')
 
-# Determine 'Exception trend' based on the sheet data we are processing
-combined_df['Exception trend'] = 'Deals'
+# Fill missing values in Volume with 0
+full_combined_df['Volume'] = full_combined_df['Volume'].fillna(0)
 
+# Merge the calculated Attribute Count and Priority Count back into the full_combined_df
+full_combined_df = pd.merge(full_combined_df, attribute_count_per_msg_type, on='MSG_TYP', how='left')
+full_combined_df = pd.merge(full_combined_df, priority_count, on='PRIORITY', how='left')
+
+# Fill missing values in Attribute Count with 0
+full_combined_df['Attribute Count'] = full_combined_df['Attribute Count'].fillna(0)
+
+# Set Month/Year to be the same for all MSG_TYP values
+full_combined_df['Month/Year'] = previous_month_name
+
+# Define banking and trading book types
 banking_book_types = ["Business Finance", "Paragon", "TAS"]
 trading_book_types = ["3D Static", "Bloomberg", "Intex", "STS"]
 
-combined_df['Message Type Group'] = combined_df['MSG_TYP'].apply(
+full_combined_df['Message Type Group'] = full_combined_df['MSG_TYP'].apply(
     lambda x: "Banking Book" if x in banking_book_types else "Trading Book" if x in trading_book_types else "Other"
 )
 
-# Reorder columns to match the provided screenshot, excluding 'Right Exception trend'
-final_df = combined_df[['Count(*)', 'Exception trend', 'MSG_TYP', 'Message Type Group', 'Attribute Count', 
-                        'PRIORITY', 'Priority Count', 'Volume', 'Month/Year', 'STATUS', 'Status_Count']]
+# Add 'Exception trend' and 'Total' columns
+full_combined_df['Exception trend'] = 'Deals'
+full_combined_df['Total'] = total_value
 
-# Load the data from the Excel file's "Q1 Tranche" sheet
-df_q1_tranche = pd.read_excel(file_path, sheet_name='Q1 Tranche')
+# Reorder and select relevant columns as per the specified order
+final_df = full_combined_df[['Exception trend', 'MSG_TYP', 'Message Type Group', 'Attribute Count', 
+                             'PRIORITY', 'Volume', 'Total', 'Month/Year', 'STATUS', 'Priority Count']]
 
-# Load the "COUNT(*)" column from the "Q2 Tranche" sheet
-df_q2_tranche = pd.read_excel(file_path, sheet_name='Q2 Tranche', usecols=['COUNT(*)'])
+# Define the path for the output Excel file
+output_file_path = 'output_data_analysis.xlsx'  # Replace with your desired file path
 
-# Get the first value from the "COUNT(*)" column of "Q2 Deal" sheet (assuming it's the same for all records)
-count_value_tranche = df_q2_tranche['COUNT(*)'].iloc[0]
+# Save the final dataframe to an Excel file
+final_df.to_excel(output_file_path, index=False)
 
-# Ensure the OPEN_DT column in "Q1 Deal" is in datetime format
-df_q1_tranche['OPEN_DT'] = pd.to_datetime(df_q1_tranche['OPEN_DT'], errors='coerce')
-
-# Use OPEN_DT for the Month/Year value for all records
-df_q1_tranche['Month/Year'] = df_q1_tranche['OPEN_DT'].dt.strftime('%b-%y')
-
-msg_typ_rename = {
-    '3DS': '3D Static',
-    'BF': 'Business Finance',
-    'BBG': 'Bloomberg'
-}
-
-df_q1_tranche['MSG_TYP'] = df_q1_tranche['MSG_TYP'].map(msg_typ_rename).fillna(df_q1_tranche['MSG_TYP'])
-
-# Group by MSG_TYP, PRIORITY, STATUS, and Month/Year, and count occurrences
-grouped_tranche = df_q1_tranche.groupby(['MSG_TYP', 'PRIORITY', 'STATUS', 'Month/Year'])
-
-# Count the number of occurrences in each group
-output_df_tranche = grouped_tranche.size().reset_index(name='Volume')
-
-# Create Status_Count column, assuming it is the same as Volume
-output_df_tranche['Status_Count'] = output_df_tranche['Volume']
-
-# Now, count distinct ATTR_NME for each MSG_TYP
-distinct_attr_count_tranche = df_q1_tranche.groupby('MSG_TYP')['ATTR_NME'].nunique().reset_index(name='Attribute Count')
-
-# Calculate Priority Count which is the count of distinct ATTR_NME for a given PRIORITY across all MSG_TYP
-priority_count_tranche = df_q1_tranche.groupby('PRIORITY')['ATTR_NME'].nunique().reset_index(name='Priority Count')
-
-# Merge the distinct attribute count with the main DataFrame
-combined_df_tranche = pd.merge(output_df_tranche, distinct_attr_count_tranche, on='MSG_TYP', how='left')
-
-# Merge the priority count with the main DataFrame
-combined_df_tranche = pd.merge(combined_df_tranche, priority_count_tranche, on='PRIORITY', how='left')
-
-# Add column 'Count(*)' with the value from "Q2 Tranche" sheet
-combined_df_tranche['Count(*)'] = count_value_tranche
-
-# Determine 'Exception trend' based on the sheet data we are processing
-combined_df_tranche['Exception trend'] = 'Tranche'
-
-banking_book_types_tranche = ["Business Finance", "Paragon", "TAS"]
-trading_book_types_tranche = ["3D Static", "Bloomberg", "Intex", "STS"]
-
-combined_df['Message Type Group'] = combined_df_tranche['MSG_TYP'].apply(
-    lambda x: "Banking Book" if x in banking_book_types_tranche else "Trading Book" if x in trading_book_types_tranche else "Other"
-)
-
-# Reorder columns to match the provided screenshot, excluding 'Right Exception trend'
-final_df_tranche = combined_df_tranche[['Count(*)', 'Exception trend', 'MSG_TYP', 'Message Type Group', 'Attribute Count', 
-                        'PRIORITY', 'Priority Count', 'Volume', 'Month/Year', 'STATUS', 'Status_Count']]
-
-final_combined_df = pd.concat([final_df, final_df_tranche], ignore_index=True)
-
-final_combined_df.rename(columns={
-    'Count(*)': 'Total'
-}, inplace=True)
-
-ordered_columns = [
-    'Exception trend', 'MSG_TYP', 'Message Type Group', 'Attribute Count', 
-    'PRIORITY', 'Volume', 'Total', 'Month/Year', 'STATUS', 'Priority Count'
-]
-final_combined_df = final_combined_df[ordered_columns]
-
-# Get unique combinations of MSG_TYP and PRIORITY
-msg_priority_combinations = final_combined_df[['MSG_TYP', 'PRIORITY']].drop_duplicates()
-
-# Create a DataFrame to hold missing rows
-missing_rows = []
-
-# Iterate over each MSG_TYP
-for msg_typ in final_combined_df['MSG_TYP'].unique():
-    # Filter the DataFrame for the current MSG_TYP
-    df_msg_typ = final_combined_df[final_combined_df['MSG_TYP'] == msg_typ]
-
-    # Check for each priority
-    for priority in ['P1', 'P2', 'P3']:
-        # Filter the DataFrame for the current priority
-        df_priority = df_msg_typ[df_msg_typ['PRIORITY'] == priority]
-
-        # If there are less than two statuses for the priority, we need to add the missing ones
-        if len(df_priority) < 2:
-            # Find which status is missing
-            existing_statuses = df_priority['STATUS'].tolist()
-            opposite_status = get_opposite_status(existing_statuses)
-
-            # Add the missing status
-            for status in ['CLOSED', 'OPEN']:
-                if status not in existing_statuses:
-                    # Create a new row with the missing status and a Volume of 0
-                    # Other column values are taken from one of the existing rows for consistency
-                    new_row = df_priority.iloc[0].copy()
-                    new_row['STATUS'] = status
-                    new_row['Volume'] = 0
-                    new_row['Status_Count'] = 0  # Assuming Status_Count should also be 0
-                    missing_rows.append(new_row)
-
-# Append missing_rows to the final_combined_df
-if missing_rows:
-    final_combined_df = final_combined_df.append(missing_rows, ignore_index=True)
-
-# Specify the output folder path and file name
-output_folder_path = '/path/to/your/output/folder/'
-output_file_name = 'final_output.xlsx'
-
-# Create the full file path
-full_file_path = os.path.join(output_folder_path, output_file_name)
-
-# Check if the output folder exists, and if not, create it
-if not os.path.exists(output_folder_path):
-    os.makedirs(output_folder_path)
-
-# Save the DataFrame to an Excel file
-final_combined_df.to_excel(full_file_path, index=False)
-
-print(f"File saved successfully to {full_file_path}")
+print(f"Data saved to {output_file_path}")
