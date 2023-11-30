b513bdc01811594dae7d17cf8a0f257ff230ba7e
SuchithG <suchithgowda1995@gmail.com>
2023-11-30 16:37:22 +0530

    feat: :art:

--- ExceptionTrends.py
+++ ExceptionTrends.py
@@ -74,6 +74,72 @@ full_combined_df['Total'] = total_value
 final_df = full_combined_df[['Exception trend', 'MSG_TYP', 'Message Type Group', 'Attribute Count', 
                              'PRIORITY', 'Volume', 'Total', 'Month/Year', 'STATUS', 'Priority Count']]
 
+df_q1_tranche = pd.read_excel(file_path, sheet_name='Q1 Tranche')
+df_q2_tranche = pd.read_excel(file_path, sheet_name='Q2 Tranche', usecols=['COUNT(*)'])
+
+# Get the first value from the "COUNT(*)" column
+total_value_tranche = df_q2_tranche['COUNT(*)'].iloc[0]
+
+# Process the data
+df_q1['OPEN_DT'] = pd.to_datetime(df_q1['OPEN_DT'], errors='coerce')
+
+msg_typ_rename_tranche = {
+    '3DS': '3D Static',
+    'BF': 'Business Finance',
+    'BBG': 'Bloomberg',
+    # Add other mappings as needed
+}
+df_q1_tranche['MSG_TYP'] = df_q1_tranche['MSG_TYP'].map(msg_typ_rename_tranche).fillna(df_q1_tranche['MSG_TYP'])
+
+# Group and count
+grouped_tranche = df_q1_tranche.groupby(['MSG_TYP', 'PRIORITY', 'STATUS']).size().reset_index(name='Volume')
+
+# Calculate the Attribute Count for each MSG_TYP
+attribute_count_per_msg_type_tranche = df_q1_tranche.groupby('MSG_TYP')['ATTR_NME'].nunique().reset_index(name='Attribute Count')
+
+# Calculate the Priority Count for each PRIORITY
+priority_count_tranche = df_q1_tranche.groupby('PRIORITY')['ATTR_NME'].nunique().reset_index(name='Priority Count')
+
+# Define MSG_TYP and PRIORITY categories
+all_msg_types_tranche = ["3D Static", "Bloomberg", "Business Finance", "Intex", "Paragon", "STS", "TAS"]
+all_priorities_tranche = ['P1', 'P2', 'P3']
+status_types_tranche = ['OPEN', 'CLOSED']
+
+# Create a dataframe with all combinations of MSG_TYP, PRIORITY, and STATUS
+all_combinations_tranche = pd.DataFrame(product(all_msg_types_tranche, all_priorities_tranche, status_types_tranche), columns=['MSG_TYP', 'PRIORITY', 'STATUS'])
+
+# Merge with the existing dataframe to ensure all combinations are present
+full_combined_df_tranche = pd.merge(all_combinations_tranche, grouped_tranche, on=['MSG_TYP', 'PRIORITY', 'STATUS'], how='left')
+
+# Fill missing values in Volume with 0
+full_combined_df_tranche['Volume'] = full_combined_df_tranche['Volume'].fillna(0)
+
+# Merge the calculated Attribute Count and Priority Count back into the full_combined_df
+full_combined_df_tranche = pd.merge(full_combined_df_tranche, attribute_count_per_msg_type_tranche, on='MSG_TYP', how='left')
+full_combined_df_tranche = pd.merge(full_combined_df_tranche, priority_count_tranche, on='PRIORITY', how='left')
+
+# Fill missing values in Attribute Count with 0
+full_combined_df_tranche['Attribute Count'] = full_combined_df_tranche['Attribute Count'].fillna(0)
+
+# Set Month/Year to be the same for all MSG_TYP values
+full_combined_df_tranche['Month/Year'] = previous_month_name
+
+# Define banking and trading book types
+banking_book_types_tranche = ["Business Finance", "Paragon"]
+trading_book_types_tranche = ["3D Static", "Bloomberg", "Intex", "STS", "TAS"]
+
+full_combined_df_tranche['Message Type Group'] = full_combined_df_tranche['MSG_TYP'].apply(
+    lambda x: "Banking Book" if x in banking_book_types_tranche else "Trading Book" if x in trading_book_types_tranche else "Other"
+)
+
+# Add 'Exception trend' and 'Total' columns
+full_combined_df_tranche['Exception trend'] = 'Deals'
+full_combined_df_tranche['Total'] = total_value_tranche
+
+# Reorder and select relevant columns as per the specified order
+final_df_tranche = full_combined_df_tranche[['Exception trend', 'MSG_TYP', 'Message Type Group', 'Attribute Count', 
+                             'PRIORITY', 'Volume', 'Total', 'Month/Year', 'STATUS', 'Priority Count']]
+
 # Define the path for the output Excel file
 output_file_path = 'output_data_analysis.xlsx'  # Replace with your desired file path
 
