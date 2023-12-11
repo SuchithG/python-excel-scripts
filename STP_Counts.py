b0b484b12cde8a2360d89e2c85df2f1b40568c02
SuchithG <suchithgowda1995@gmail.com>
2023-12-11 13:21:25 +0530

    feat: :art:

--- STP_Counts.py
+++ STP_Counts.py
@@ -1,8 +1,23 @@
 import pandas as pd
+from datetime import datetime, timedelta
+
+# Get the current date
+current_date = datetime.now()
+
+# Get the first day of the current month
+first_day_of_current_month = current_date.replace(day=1)
+
+# Calculate the last day of the previous month
+last_day_of_previous_month = first_day_of_current_month - timedelta(days=1)
+
+# Get the month and year of the previous month
+previous_month = last_day_of_previous_month.strftime('%b_%Y')
+
+# Construct the file name
+file_name = f"iRDS_SQL_Query_{previous_month}.xlsx"
 
 # Load the Excel file
-excel_file = 'your_file.xlsx'  # Replace 'your_file.xlsx' with your file name
-xls = pd.ExcelFile(excel_file)
+xls = pd.ExcelFile(file_name)
 
 # Assuming the first sheet is 'Table1' containing the data for the first table
 # Change these names according to your Excel file
