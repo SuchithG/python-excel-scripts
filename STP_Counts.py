13c1f42779ee8567303ec7969fb6124b4c2d48d9
SuchithG <suchithgowda1995@gmail.com>
2023-12-11 13:20:56 +0530

    feat: :art:

--- STP_Counts.py
+++ STP_Counts.py
@@ -4,39 +4,15 @@ import pandas as pd
 excel_file = 'your_file.xlsx'  # Replace 'your_file.xlsx' with your file name
 xls = pd.ExcelFile(excel_file)
 
-# Assuming you have three sheets named 'Table1', 'Table2', and 'Table3'
+# Assuming the first sheet is 'Table1' containing the data for the first table
 # Change these names according to your Excel file
 table1 = pd.read_excel(xls, 'Table1')
-table2 = pd.read_excel(xls, 'Table2')
-table3 = pd.read_excel(xls, 'Table3')
 
-# Perform calculations on each table
-# Replace these calculations with your desired operations
+# Calculate 'Total Nov exception' by summing up values across columns
+total_nov_exception = table1[['Equity', 'Loans', 'LD', 'FI']].sum(axis=1)
 
-# Table 1 calculations
-table1_sum = table1.sum()  # Example: calculating sum of columns
-table1_mean = table1.mean()  # Example: calculating mean of columns
+# Add 'Total' column to the table
+table1['Total'] = total_nov_exception
 
-# Table 2 calculations
-table2_sum = table2.sum()  # Example: calculating sum of columns
-table2_mean = table2.mean()  # Example: calculating mean of columns
-
-# Table 3 calculations
-table3_sum = table3.sum()  # Example: calculating sum of columns
-table3_mean = table3.mean()  # Example: calculating mean of columns
-
-# Print the calculated results
-print("Table 1 Sum:")
-print(table1_sum)
-print("\nTable 1 Mean:")
-print(table1_mean)
-
-print("\nTable 2 Sum:")
-print(table2_sum)
-print("\nTable 2 Mean:")
-print(table2_mean)
-
-print("\nTable 3 Sum:")
-print(table3_sum)
-print("\nTable 3 Mean:")
-print(table3_mean)
+# Display the modified table with the 'Total' column
+print(table1)
