0179d79ed983095638beb68dcfc4c3150fdd4348
SuchithG <suchithgowda1995@gmail.com>
2024-02-13 13:43:27 +0530

    refactor:

--- send_email.py
+++ send_email.py
@@ -3,7 +3,6 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
-from email import encoders
 import pandas as pd
 from datetime import datetime, timedelta
 
